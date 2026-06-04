5e7ba19c0243ae7279721823cfc17eefc3a6753e
sansansint <sansansint@example.com>
2026-06-04 14:25:01 +0800

    added dagster dev

--- listenbrainz_dagster/listenbrainz_dagster/schedules.py
+++ listenbrainz_dagster/listenbrainz_dagster/schedules.py
@@ -6,10 +6,15 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from .assets import listenbrainz_gcp_dbt_assets
 
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [listenbrainz_gcp_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+     build_schedule_from_dbt_selection(
+         [listenbrainz_gcp_dbt_assets],
+         job_name="materialize_dbt_models",
+         cron_schedule="0 0 * * *",
+         dbt_select="fqn:*",
+     ),
 ]
+
+# Access the job object created by the schedule
+materialize_dbt_models_job = schedules[0].job
+schedules = [schedules[0]]
+jobs = [materialize_dbt_models_job]
